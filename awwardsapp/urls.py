d17bca1efd5d77dbdbcbc413dfdf6c7d11c7f1e1
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-12-13 06:27:02 +0300

    Add the new project and search urls

--- awwardsapp/urls.py
+++ awwardsapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
     url('^$',views.index,name = 'index'),
     url(r'^profile/', views.profile, name='profile'),
-
+    url(r'^new/project$', views.new_project, name='newProject'),
+    url('search/', views.search, name='search_results'),
 ]
