52569bf66c6fe56b8323c6c7f6a30053b2645ac6
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-12-13 06:19:04 +0300

    Create new project form

--- awwardsapp/forms.py
+++ awwardsapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from django.db.models.fields import TextField
 from django.forms.widgets import Textarea
-from .models import Profile
+from .models import Profile,Project
 
 class ProfileForm(forms.ModelForm):
     class Meta:
@@ -8,4 +9,11 @@ class ProfileForm(forms.ModelForm):
         fields = ['bio','profile_pic','contact']
         widgets = {
             'bio': Textarea(attrs={'cols': 30, 'rows': 3}),
+        }
+class NewProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ['title', 'image','description','link']
+        widgets = {
+            'description': Textarea(attrs={'cols' : 20, 'rows' : 5}),
         }
