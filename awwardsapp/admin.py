a1f1b051cf4b3db739dd92b4568f9dc88575ca1d
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-12-14 07:26:36 +0300

    Register the models

--- awwardsapp/admin.py
+++ awwardsapp/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from awwardsapp.models import Profile,Project
+from awwardsapp.models import Profile,Project,Review,Rating
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Project)
+admin.site.register(Review)
+admin.site.register(Rating)
+
+
 
