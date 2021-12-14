c33b491bda9df1239d36370d6b301e4027b58780
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-12-14 08:02:43 +0300

    Add paths and modify the urls to paths

--- awwardsapp/urls.py
+++ awwardsapp/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns=[
-    url('^$',views.index,name = 'index'),
-    url(r'^profile/', views.profile, name='profile'),
-    url(r'^new/project$', views.new_project, name='newProject'),
-    url('search/', views.search, name='search_results'),
+    path('',views.index,name = 'index'),
+    path('profile/', views.profile, name='profile'),
+    path('new/project/', views.new_project, name='newProject'),
+    path('search/', views.search, name='search_results'),
+    path('reviews/',views.reviews,name = 'reviews'),
+    path('rate/<int:id>',views.rate, name='rating'),
+    path("project/<int:project_id>/", views.project_review, name="project_review"),
+
 ]
